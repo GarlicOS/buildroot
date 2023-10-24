989d6f9ada8d5a870e9546d37e149a8e42c1f2cc
Black-Seraph <admin@black-seraph.com>
2023-10-24 15:13:53 +0200

    Updated tic80 core * Had to pull in the curlpp package due to tic80's new curl dependency * libuv is no longer required * tic80 can now be compiled with fast-math enabled

--- package/tic80/tic80.mk
+++ package/tic80/tic80.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TIC80_VERSION = 067d90d2e2380540398b0867582f327dbea13832
+TIC80_VERSION = 0937023babce9dd55eba5ada4b46c2f98b740324
 TIC80_SITE = https://github.com/nesbox/TIC-80.git
 TIC80_SITE_METHOD = git
 TIC80_GIT_SUBMODULES = YES
@@ -24,10 +24,7 @@ define TIC80_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/root/.config/retroarch/cores
 	wget -O $(TARGET_DIR)/root/.config/retroarch/cores/tic80_libretro.info https://raw.githubusercontent.com/libretro/libretro-super/master/dist/info/tic80_libretro.info
 	$(INSTALL) -m 0755 -D $(@D)/lib/libjanet.so $(TARGET_DIR)/usr/lib/
-	$(INSTALL) -m 0755 -D $(@D)/lib/libuv.so.1.0.0 $(TARGET_DIR)/usr/lib/
 	$(INSTALL) -m 0755 -D $(@D)/lib/libzip.so $(TARGET_DIR)/usr/lib/
-	ln -s libuv.so.1.0.0 $(TARGET_DIR)/usr/lib/libuv.so.1
-	ln -s libuv.so.1 $(TARGET_DIR)/usr/lib/libuv.so
 	$(INSTALL) -m 0755 -D $(@D)/lib/tic80_libretro.so $(TARGET_DIR)/root/.config/retroarch/cores/
 endef
 
