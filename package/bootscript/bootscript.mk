619eda15fc07d4c811b019c0e13ad8cd28518460
Black-Seraph <admin@black-seraph.com>
2024-05-25 18:37:36 +0200

    Added EGL/GLES driver hotswap support * As well as a RG35XX+ compatible armhf driver

--- package/bootscript/bootscript.mk
+++ package/bootscript/bootscript.mk
@@ -10,6 +10,10 @@ BOOTSCRIPT_SITE_METHOD = local
 
 define BOOTSCRIPT_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/init $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/drivers/usr/lib/* $(TARGET_DIR)/usr/lib
+	ln -sf rg35xx+_libmali.so $(TARGET_DIR)/usr/lib/rg35xx+_libEGL.so
+	ln -sf rg35xx+_libmali.so $(TARGET_DIR)/usr/lib/rg35xx+_libGLESv1_CM.so
+	ln -sf rg35xx+_libmali.so $(TARGET_DIR)/usr/lib/rg35xx+_libGLESv2.so
 endef
 
 $(eval $(generic-package))
